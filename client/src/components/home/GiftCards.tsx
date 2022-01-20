5399ae438e53b5c92a172105ef2277a6ebcce319
Ben Lammers <21benlammers@gmail.com>
2022-01-20 09:11:01 -0500

    Added gift card and nayax graphics

--- client/src/components/home/GiftCards.tsx
+++ client/src/components/home/GiftCards.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { GiftCardsQuery } from '../../generated/types';
 import { PhoneIcon } from '../icons/PhoneIcon';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export const GiftCards: React.FC = () => {
    const { sanityHome, sanityInfo } = useStaticQuery<GiftCardsQuery>(query);
@@ -12,7 +13,10 @@ export const GiftCards: React.FC = () => {
             <h2 className="text-hosers-red font-bold text-lg">{sanityHome.giftCards.name}</h2>
             <span className="text-hosers-gray font-bold text-2xl">{sanityHome.giftCards.title}</span>
          </div>
-         <p className="text-hosers-gray text-lg">{sanityHome.giftCards.description}</p>
+         <div className="grid gap-1 grid-cols-[1fr_max-content]">
+            <p className="text-hosers-gray text-lg">{sanityHome.giftCards.description}</p>
+            <StaticImage src="../../images/giftcards.png" alt="Two Hosers gift cards" className="w-36" />
+         </div>
          <a className="btn-red" href={`tel:+1${sanityInfo.phoneNumber}`}>
             <span>Phone Us</span>
             <PhoneIcon className="w-5 h-5" />
--- client/src/components/home/Payment.tsx
+++ client/src/components/home/Payment.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { PaymentQuery } from '../../generated/types';
 import { ExternalIcon } from '../icons/ExternalIcon';
+import { Nayax } from '../util/Nayax';
 
 export const Payment: React.FC = () => {
    const { sanityHome, sanityInfo } = useStaticQuery<PaymentQuery>(query);
@@ -12,7 +13,11 @@ export const Payment: React.FC = () => {
             <h2 className="text-hosers-blue font-bold text-lg">{sanityHome.payment.name}</h2>
             <span className="text-hosers-gray font-bold text-2xl">{sanityHome.payment.title}</span>
          </div>
-         <p className="text-hosers-gray text-lg">{sanityHome.payment.description}</p>
+
+         <div className="grid gap-2 grid-cols-[1fr_max-content]">
+            <p className="text-hosers-gray text-lg">{sanityHome.payment.description}</p>
+            <Nayax className="w-24 -translate-y-4" />
+         </div>
          <a className="btn-blue" href={sanityInfo.erecieptLink}>
             <span>Get E-Receipts</span>
             <ExternalIcon className="w-5 h-5" />
